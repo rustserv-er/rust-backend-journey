b2db122503d77a750ebf70a829e8286366303cac
SArvesh <baranwalrajesh573@gmail.com>
2025-08-17 19:19:01 +0530

    day 1 learnt about variable and function

---
+++ examples/function.rs
@@ -0,0 +1,16 @@
+fn add(x:u32,y:u32)->u32{
+    return x+y;
+}
+
+pub fn div(x:u32,y:u32)->u32{
+    return x*y;
+}
+pub fn mul(x:u32,y:u32)->u32{
+    return x/y;
+}
+
+fn main(){
+    println!("{}",add(4,5));
+    println!("{}",div(4,5));
+    println!("{}",mul(4,5));
+}
